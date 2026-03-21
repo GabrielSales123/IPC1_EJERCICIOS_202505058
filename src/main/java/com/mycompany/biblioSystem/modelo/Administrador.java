6f669176dd1cf8b61e1c8577e2fd8f5832e7006a
GabrielSales123 <gabrielandres.sales2007@gmail.com>
2026-03-21 00:48:38 -0600

    Delete src/main directory

--- src/main/java/com/mycompany/biblioSystem/modelo/Administrador.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.biblioSystem.modelo;
-
-
-public class Administrador extends Usuario{
-    public Administrador(String id, String nombre, String password){
-        super(id, nombre, password, "ADMIN");
-    }
-}
